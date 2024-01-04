f9a29da37df1ae40dbd1c85c9940994f17a56b7f
Beatriz Cavalcanti <beatriz.cavalcanti@yahoo.com>
2024-01-03 22:49:22 -0300

    Código de entrada de dados e saida com comentarios

--- Banco.java
+++ Banco.java
@@ -1,31 +1,36 @@
 import java.util.Scanner;
 
+// objetivo: pegar dados do cliente (nome, agencia, número da conta e saldo) e retornar esses dados organzados.
 public class Banco{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+            // Declaração das variáveis
 
-            int numeroConta; //pega o número da agencia do cliente 
+            int numeroConta;
             String agencia, nomeCliente;
             double saldo;
 
 
-
+        // Solicitação do nome do cliente
         System.out.println("Olá, Por favor, digite o seu nome! ");
         nomeCliente = sc.nextLine();
 
+        // Solicitação do número da conta
         System.out.println("Por favor, digite o número da conta! ");
         numeroConta = sc.nextInt();
 
         sc.nextLine(); // Consumindo a quebra de linha pendente
 
+        // Solicitação do número da agência
         System.out.println("Por favor, digite o número da Agência! ");
         agencia = sc.nextLine();  
         
+        // Solicitação do saldo
         System.out.println("Por favor, digite o seu saldo! ");
         saldo = sc.nextDouble();
 
-
+        // Mensagem de boas-vindas e informações da conta
         System.out.println("Seja bem-vindo " + nomeCliente + "! Obrigado por criar uma conta em nosso banco! sua agência é " + agencia + " conta " + numeroConta + " e o seu saldo R$ " + saldo + " já está disponível para saque!");
     }
 }
